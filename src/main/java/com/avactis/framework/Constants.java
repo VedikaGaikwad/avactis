239ba7beed7996ff60251d9fd0f26b0212af600b
Vedika Gaikwad <vedikagaikwad2703@gmail.com>
2024-08-04 23:21:13 +0530

    Added Constants class

---
+++ src/main/java/com/avactis/framework/Constants.java
@@ -0,0 +1,7 @@
+package com.avactis.framework;
+
+public class Constants {
+
+	public static final String BASE_URL = "http://localhost/Avactis";
+    public static final String BROWSER = "chrome";
+}
